2f0210695edc7aff277a854a93b4b068a29a68a1
podpolkovnik <paw.1999@mail.ru>
2020-03-04 15:53:27 +0300

    add tests

--- lab5/Main.java
+++ lab5/Main.java
@@ -2,8 +2,18 @@ package lab5;
 
 public class Main {
     public static void main(String[] args) {
+        Movable mainWindow = new MainWindow();
+        mainWindow.moveTo(123, 123);
+        System.out.println(mainWindow.getX());
+        System.out.println(mainWindow.getY());
+
+        Point point = new Point();
+        point.moveTo(500, 500);
+        point.draw();
+
         Background back = new Background();
         back.setColor("black");
+        back.setTexture("stone");
         back.draw();
     }
 }
