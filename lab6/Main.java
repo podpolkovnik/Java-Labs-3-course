27e983b3faca8c1401b3aa417190e4e9906b6aaf
podpolkovnik <paw.1999@mail.ru>
2020-03-04 20:50:08 +0300

    add complete lab6

--- lab6/Main.java
+++ lab6/Main.java
@@ -1,8 +1,52 @@
 package lab6;
+
 import java.io.*;
+import java.util.Random;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        
+        File sin = new File("./lab6/sin.txt");
+        File input = new File("./lab6/input.txt");
+        try {
+            sin.createNewFile();
+            input.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Random rand = new Random();
+            PrintWriter sinWriter = new PrintWriter(sin);
+            PrintWriter inputWriter = new PrintWriter(input);
+            for (int i = 0; i < 361; ++i) {
+                sinWriter.println(Math.sin(i));
+            }
+            inputWriter.println(rand.nextInt(360));
+            sinWriter.close();
+            inputWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader sinReader = new BufferedReader(new FileReader(sin));
+            BufferedReader inputReader = new BufferedReader(new FileReader(input));
+
+            Vector<Double> sinValues = new Vector<Double>();
+
+            String line;
+			while((line = sinReader.readLine()) != null) {
+                double sinValue = Double.parseDouble(line);
+                sinValues.add(sinValue);
+            }
+            int degree = Integer.parseInt(inputReader.readLine());
+
+            System.out.println(sinValues.get(degree));
+            
+            sinReader.close();
+            inputReader.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
