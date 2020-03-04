59dcb35f86773d16ed82e9ca303d0261b2f3b332
podpolkovnik <paw.1999@mail.ru>
2020-03-04 15:00:38 +0300

    add files

---
+++ lab5/Main.java
@@ -0,0 +1,9 @@
+package lab5;
+import LabPackets.ru.novsu.povt.vpg.graphics.*;
+public class Main {
+    public static void main(String[] args) {
+        Background back = new Background();
+        back.setColor("black");
+        back.draw();
+    }
+}
