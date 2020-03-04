55dfe0d485ad92772dfd967b132e4b03e8235e0f
podpolkovnik <paw.1999@mail.ru>
2020-03-04 15:04:18 +0300

    upload files from lab3

---
+++ lab3/Point.java
@@ -0,0 +1,23 @@
+package lab3;
+
+public class Point extends Graph {
+    protected double x, y;
+    private String color = "none";
+    @Override public void draw() {
+        System.out.println("Точка: x = " + x +  ", y = " + y);
+    }
+    public void moveTo(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void move(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return this.color;
+    }
+}
