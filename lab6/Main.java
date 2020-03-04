df3cca4aa05877c6f695616366e39a966adfddaa
podpolkovnik <paw.1999@mail.ru>
2020-03-04 21:34:27 +0300

    remove try/catch cases

--- lab6/Main.java
+++ lab6/Main.java
@@ -5,59 +5,53 @@ import java.util.Random;
 import java.util.Vector;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException {
         File sin = new File("./lab6/sin.txt");
         File input = new File("./lab6/input.txt");
-        try {
-            sin.createNewFile();
-            input.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
+        sin.createNewFile();
+        input.createNewFile();
+        Random rand = new Random();
+        PrintWriter sinWriter = new PrintWriter(sin);
+        PrintWriter inputWriter = new PrintWriter(input);
+        for (int i = 0; i < 361; ++i) {
+            sinWriter.println(Math.sin(i));
         }
-        try {
-            Random rand = new Random();
-            PrintWriter sinWriter = new PrintWriter(sin);
-            PrintWriter inputWriter = new PrintWriter(input);
-            for (int i = 0; i < 361; ++i) {
-                sinWriter.println(Math.sin(i));
-            }
-            inputWriter.println(rand.nextInt(360));
-            sinWriter.close();
-            inputWriter.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        inputWriter.println(rand.nextInt(360));
+        sinWriter.close();
+        inputWriter.close();
+        BufferedReader sinReader = new BufferedReader(new FileReader(sin));
+        BufferedReader inputReader = new BufferedReader(new FileReader(input));
+
+        Vector<Double> sinValues = new Vector<Double>();
+
+        String line;
+        while((line = sinReader.readLine()) != null) {
+            double sinValue = Double.parseDouble(line);
+            sinValues.add(sinValue);
         }
-        try {
-            BufferedReader sinReader = new BufferedReader(new FileReader(sin));
-            BufferedReader inputReader = new BufferedReader(new FileReader(input));
+        int degree = Integer.parseInt(inputReader.readLine());
 
-            Vector<Double> sinValues = new Vector<Double>();
+        System.out.println(sinValues.get(degree));
+        
+        sinReader.close();
+        inputReader.close();
 
-            String line;
-			while((line = sinReader.readLine()) != null) {
-                double sinValue = Double.parseDouble(line);
-                sinValues.add(sinValue);
-            }
-            int degree = Integer.parseInt(inputReader.readLine());
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("./lab6/sin2.dat"));
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("./lab6/sin2.dat"));
+        objectOutputStream.writeObject(sinValues);
+        objectOutputStream.writeObject(sinValues);
+        Vector<Double> vector = new Vector<Double>();
+        Object readedValue = objectInputStream.readObject();
 
-            System.out.println(sinValues.get(degree));
-            
-            sinReader.close();
-            inputReader.close();
-
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("./lab6/sin2.dat"));
-            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("./lab6/sin2.dat"));
-            objectOutputStream.writeObject(sinValues);
-            Vector<Double> vector = new Vector<Double>();
-            vector = (Vector<Double>) objectInputStream.readObject();
+        if (readedValue instanceof Vector<?>) {
+            vector = (Vector<Double>) readedValue;
             System.out.println(vector);
-
-            objectOutputStream.close();
-            objectInputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         }
+        else {
+            System.out.println("Error, unable to convert data");
+        }
+
+        objectOutputStream.close();
+        objectInputStream.close();
     }
 }
